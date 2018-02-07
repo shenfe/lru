188daae67d1f86321196ac1a19bf1e36cd5ac91f
hengwu <youchuangqiba@163.com>
2018-02-08 01:25:35 +0800

    upd8

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var LRU = function (config) {
     if (!(this instanceof LRU)) return new LRU(config);
     config = config || {};
+    if (typeof config === 'number') config = { maxSize: config };
     var maxSize = config.maxSize || 100;
     var onPop = config.onPop || function (key, val) {};
     var keyNodes = {};
@@ -16,18 +17,20 @@ var LRU = function (config) {
         var tailNode = keyNodes[tailKey];
         onPop(tailKey, tailNode.value);
         size--;
-        if (tailNode.prev) {
+        delete keyNodes[tailKey];
+        if (tailNode.prev != null) {
             tailKey = tailNode.prev;
             keyNodes[tailKey].next = null;
         } else {
             tailKey = headKey = null;
         }
+        free(tailNode);
     };
-    this.set = function (key, val) {
+    var set = function (key, val) {
         if (have(key)) {
             var node = keyNodes[key];
             node.value = val;
-            if (key === headKey) return;
+            if (key === headKey) return val;
             var prevNode = keyNodes[node.prev];
             if (key === tailKey) {
                 tailKey = node.prev;
@@ -42,12 +45,11 @@ var LRU = function (config) {
             node.next = headKey;
             keyNodes[headKey].prev = key;
             headKey = key;
-            return;
+            return val;
         }
         /* If the key doesn't exist */
         size++;
         var node = {
-            // key: key,
             value: val,
             prev: null,
             next: null
@@ -63,6 +65,71 @@ var LRU = function (config) {
         if (size > maxSize) {
             pop();
         }
+        return val;
     };
-    this.get = function (key) {};
+    var free = function (node) {
+        node.value = null;
+        node.prev = null;
+        node.next = null;
+    };
+    var valueOf = function (key) {
+        if (!have(key)) return;
+        return keyNodes[key].value;
+    };
+    var get = function (key) {
+        if (!have(key)) return;
+        var val = valueOf(key);
+        set(key, val);
+        return val;
+    };
+    var remove = function (key) {
+        if (!have(key)) return false;
+        var node = keyNodes[key];
+        size--;
+        delete keyNodes[key];
+        if (node.next == null) { /* It is the tail */
+            if (node.prev == null) { /* It is also the head */
+                headKey = tailKey = null;
+            } else { /* It is not the head */
+                tailKey = node.prev;
+                keyNodes[tailKey].next = null;
+            }
+        } else { /* It is not the tail */
+            if (node.prev == null) { /* It is the head */
+                headKey = node.next;
+                keyNodes[headKey].prev = null;
+            } else { /* It is not the head */
+                keyNodes[node.prev].next = node.next;
+                keyNodes[node.next].prev = node.prev;
+            }
+        }
+        free(node);
+    };
+    var clear = function () {
+        keyNodes = {};
+        size = 0;
+        headKey = null;
+        tailKey = null;
+    };
+    var keySet = function () {
+        var keys = [];
+        var p = tailKey;
+        while (p != null) {
+            keys.push(p);
+            p = keyNodes[p].prev;
+        }
+        return keys;
+    };
+
+    this.set = set;
+    this.val = valueOf;
+    this.get = get;
+    this.remove = remove;
+    this.clear = clear;
+    this.size = function () {
+        return size;
+    };
+    this.keys = keySet;
 };
+
+module.exports = LRU;
